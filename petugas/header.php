09c19de1d4ef03993288f3a4cbd8313c224d6d4e
ravidianfahrezi <153307266+ravidianfahrezi@users.noreply.github.com>
2024-02-29 07:28:54 +0700

    header.php

--- petugas/header.php
+++ petugas/header.php
@@ -12,7 +12,7 @@ if($_SESSION['level'] != "2"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/bootstrap-5.3.2/dist/css/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="shortcut icon" href="../assets/login.png" type="image/x-icon">
     <title>Halaman Petugas</title>
 </head>
